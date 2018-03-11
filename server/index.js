a2240dad2c78ed4ccfb71dee50ec64e72814c376
Tristan Jacobs <tristan@iamt.nl>
2018-03-11 21:38:01 +0100

    working images + detail page

--- server/index.js
+++ server/index.js
@@ -3,23 +3,27 @@
 var express = require('express')
 var db = require('../db')
 var helpers = require('./helpers')
+var path = require('path')
 
 module.exports = express()
   .set('view engine', 'ejs')
   .set('views', 'view')
   .use(express.static('static'))
-  // TODO: Serve the images in `db/image` on `/image`.
+  .use('/image', express.static('db/image'))
   .get('/', all)
+  .get('/:id', detail)
   /* TODO: Other HTTP methods. */
   // .post('/', add)
-  // .get('/:id', get)
   // .put('/:id', set)
   // .patch('/:id', change)
   // .delete('/:id', remove)
   .listen(1902)
 
 function all(req, res) {
-  var result = {errors: [], data: db.all()}
+  var result = {
+    errors: [],
+    data: db.all()
+  }
 
   /* Use the following to support just HTML:  */
   res.render('list.ejs', Object.assign({}, result, helpers))
@@ -29,4 +33,27 @@ function all(req, res) {
   //   json: () => res.json(result),
   //   html: () => res.render('list.ejs', Object.assign({}, result, helpers))
   // })
+}
+
+function detail(req, res) {
+  var id = req.params.id
+
+  if (!id) {
+    return
+  }
+
+  if (db.has(id)) {
+    var result = {
+      data: db.get(id)
+    }
+    res.render('detail.ejs', Object.assign({}, result, helpers))
+  } else {
+    var errors = [{
+      title: 'Not Found',
+      id: 404,
+      detail: 'Whoops! We could not find this animal :('
+    }]
+    res.render('error.ejs', { errors: errors })
+  }
+
 }
